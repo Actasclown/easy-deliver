e686530da89e9fc8b9766c214c96515911ccf9b6
Actasclown <heyunqi@pku.edu.cn>
2017-05-24 13:59:46 +0800

    bufimage to image

--- src/main/java/com/heyunqi/easydeliver/client/ClientHandler.java
+++ src/main/java/com/heyunqi/easydeliver/client/ClientHandler.java
@@ -5,6 +5,7 @@ import com.heyunqi.easydeliver.ui.ImageViewDialog;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 
@@ -24,6 +25,7 @@ public class ClientHandler extends SimpleChannelInboundHandler<byte[]> {
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, byte[] bytes) throws Exception {
         //bytes里面就是图片的数据。
         BufferedImage image = ImageUtil.bytesToImage(bytes);
-        imageViewDialog.updateLabel(image);
+        Image tmp = image.getScaledInstance(Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height, Image.SCALE_DEFAULT);
+        imageViewDialog.updateLabel(ImageUtil.toBufferedImage(tmp));
     }
 }
